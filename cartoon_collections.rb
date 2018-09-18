bed4bfcfdc784d9ca7d804948535a1db20e1bb82
Carson Rey <c.rey494@gmail.com>
2018-09-18 01:27:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |element|
-  element.include?(cheese_types[0])
+  cheese_types.include?(element)
   end
 end
