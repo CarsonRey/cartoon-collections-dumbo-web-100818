61616c7cdc755615c9d3c6be54a98a95abea7695
Carson Rey <c.rey494@gmail.com>
2018-09-18 00:52:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ def find_the_cheese(array)
   there_is_cheese = array.find do |element|
     element.include?(cheese_types.to_s)
   end
-  if true 
-      there_is_cheese
+  if there_is_cheese == true 
+      
     else
       nil
 end
